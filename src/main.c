ccc04d8d87a51b07e98e11fcbd4af9f3acf53bc6
tolfreedom <tolfreetroy90@gmail.com>
2019-08-22 20:10:07 -0700

    first commit

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 
 #include <stdio.h>
-
 #include "ww.h"
 #include "ball.h"
 #include "player_one.h"
@@ -21,8 +20,8 @@ int main( int argc, char * argv[] ) {
 	init_player_one() ; 
 	init_player_two() ; 
 	
-	int y_dir = 3 ;
-	int x_dir = 3 ;
+	int y_dir = -3 ;
+	int x_dir = -3;
 	
 	player_two->pad_x = screen_width - player_two->width ;
 		ball->pad_x = (screen_width / 2) - (ball->width /2) ; 
@@ -32,54 +31,70 @@ int main( int argc, char * argv[] ) {
 	while(!ww_window_received_quit_event()) {
 		ww_window_update_events();	
 		
+		if (keystate.esc == 1){
+			printf ("ESC pressed\n") ;
+			ww_window_send_quit_event() ; 
+			}
+		
 		//player 1 move up
 		if (keystate.w == 1 && player_one->pad_y > 0 ) {
 			player_one->pad_y = player_one->pad_y -3 ;
 		}
+		
 		//player 1 move down
-		if (keystate.s == 1 && player_one->pad_y < screen_height - player_one->height) { 
+		if (keystate.s == 1 && player_one->pad_y < screen_height - 
+		player_one->height) { 
 			player_one->pad_y = player_one->pad_y +3 ;
 		}
+	
 		//player 2 move up
 		if (keystate.up == 1 && player_two->pad_y > 0) {
 			player_two->pad_y = player_two->pad_y -3 ;
 		}
+	
 		//player 2 move down
-		if (keystate.dn == 1 && player_two->pad_y < screen_height - player_two->height) {
+		if (keystate.dn == 1 && player_two->pad_y < screen_height - 
+			player_two->height) {
 			player_two->pad_y = player_two->pad_y +3 ;
 		}	
 		
+		
+		//ball movement
 		ball->pad_y = ball->pad_y + y_dir;
 		ball->pad_x = ball->pad_x + x_dir;
 		
-		if (ball->pad_y > screen_height - ball->height || ball->pad_y < 0){
+		if (ball->pad_y > screen_height - ball->height || 
+		ball->pad_y < 0){
 			y_dir = y_dir * -1;
 		}
-		//if (ball->pad_y < 0){
-			//y_dir = y_dir * -1;
-		//}
-		//move ball back to cent if off left side
-		if (ball->pad_x < 0 || ball->pad_x > screen_width){
+		
+		//move ball back to sent if off left side
+		if (ball->pad_x < 0  || ball->pad_x > screen_width){
 			ball->pad_x = (screen_width / 2) - (ball->width /2);
 			ball->pad_y = (screen_height /2) - (ball->height /2);
+			
 		}
-		//move ball back to center if off right side
-		//if (ball->pad_x > screen_width){
-			//ball->pad_x = (screen_width / 2) - (ball->width /2);
-			//ball->pad_y = (screen_height /2) - (ball->height /2);
-		//}
 		
-		//player 1 when reached bottom of screen
-		//if (player_one->pad_y > screen_height){
-		//	player_one->pad_y = 0 ;
-		
-		
-		
-		
+		if (player_one->pad_y < ball->pad_y && ball->pad_x < 0 + player_one->width){
+			x_dir = x_dir * -1;
+		}
+		if (player_one->pad_y + player_one->height < ball->pad_y + ball->height && ball->pad_x < 0 + player_one->width){
+			x_dir = x_dir * -1;
+		}
+		if (player_two->pad_y < ball->pad_y && ball->pad_x > screen_width - player_two->width *2){
+			x_dir = x_dir * -1;
+		}
+		if (player_two->pad_y + player_two->height < ball->pad_y + ball->height && ball->pad_x > screen_width - player_two->width *2){
+			x_dir = x_dir * -1;		
+
+		}
+			  
+			
 		ww_draw_sprite(player_one) ; 
 		ww_draw_sprite(player_two) ; 
 		ww_draw_sprite(ball) ; 
-	
+			
+
 		
 		
 		ww_window_update_buffer();
