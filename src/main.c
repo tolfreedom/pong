a9e9d8829867ad5d5d459dff27fb6836e797cea2
tolfreedom <tolfreetroy90@gmail.com>
2019-08-29 20:04:18 -0700

    poor attempt at scores

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 #include "ball.h"
 #include "player_one.h"
 #include "player_two.h"
+#include "countdown.h"
+#include "score_one.h"
+#include "score_two.h"
 
 int screen_width = 1024;
 int screen_height = 576;
@@ -19,6 +22,10 @@ int main( int argc, char * argv[] ) {
 	init_ball() ; 
 	init_player_one() ; 
 	init_player_two() ; 
+	init_countdown() ;
+	init_score_one();
+	init_score_two();
+	
 	
 	int y_dir = -3 ;
 	int x_dir = -3;
@@ -26,6 +33,9 @@ int main( int argc, char * argv[] ) {
 	player_two->pad_x = screen_width - player_two->width ;
 		ball->pad_x = (screen_width / 2) - (ball->width /2) ; 
 		ball->pad_y = (screen_height / 2) - (ball->height /2);
+		
+	score_one->pad_x = screen_width / 4 * 2;
+	
 	
 	
 	while(!ww_window_received_quit_event()) {
@@ -68,34 +78,38 @@ int main( int argc, char * argv[] ) {
 			y_dir = y_dir * -1;
 		}
 		
-		//move ball back to sent if off left side
+		//move ball back to sent if off side
 		if (ball->pad_x < 0  || ball->pad_x > screen_width){
 			ball->pad_x = (screen_width / 2) - (ball->width /2);
 			ball->pad_y = (screen_height /2) - (ball->height /2);
-			
 		}
 		
-		if (player_one->pad_y < ball->pad_y && ball->pad_x < 0 + player_one->width){
+		if (player_one->pad_y <= ball->pad_y && ball->pad_x < player_one->width){
 			x_dir = x_dir * -1;
 		}
-		if (player_one->pad_y + player_one->height < ball->pad_y + ball->height && ball->pad_x < 0 + player_one->width){
+		if (player_one->pad_y + player_one->height <= ball->pad_y + ball->height && ball->pad_x < 0 + player_one->width){
 			x_dir = x_dir * -1;
 		}
-		if (player_two->pad_y < ball->pad_y && ball->pad_x > screen_width - player_two->width *2){
+		if (player_two->pad_y < ball->pad_y && ball->pad_x  > screen_width - player_two->width *2){
 			x_dir = x_dir * -1;
-		}
-		if (player_two->pad_y + player_two->height < ball->pad_y + ball->height && ball->pad_x > screen_width - player_two->width *2){
+		}	
+		if (player_two->pad_y + player_two->height <= ball->pad_y + ball->height && ball->pad_x > screen_width - player_two->width *2){
 			x_dir = x_dir * -1;		
 
 		}
-			  
+		
+		//score
+		if (ball->pad_x > 0){
+			(score_one-> active_animationn = score_one-> active_animation + 1); 
+		}
 			
 		ww_draw_sprite(player_one) ; 
 		ww_draw_sprite(player_two) ; 
 		ww_draw_sprite(ball) ; 
-			
-
+		ww_draw_sprite(score_one) ; 
+		ww_draw_sprite(score_two) ; 
 		
+			
 		
 		ww_window_update_buffer();
 	}
